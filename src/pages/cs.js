08296a74cf352f845ac992787822dafa27568269
MaelRuffini <mael.lucien.ruffini@gmail.com>
2022-05-15 16:25:17 +0200

    commit

--- src/pages/cs.js
+++ src/pages/cs.js
@@ -24,104 +24,56 @@ function caseStudy()
     })
 
 
-    /* Swiper
-    **************************************************************/
-    var swiperCs = Swiper;
-    var init = false;
+    const swiperCs = new Swiper('.swiper--cs', {
+      direction: 'vertical',
+      noSwipingClass: 'cs__video__image-container',
+      mousewheel: false,
+      speed: 800,
+      keyboard: {
+          enabled: true,
+        },
+        effect: 'slide',
+        parallax: true,
+    })
 
 
-    function swiperMode() {
-      let mobile = window.matchMedia('(min-width: 0px) and (max-width: 768px)');
-      let tablet = window.matchMedia('(min-width: 769px) and (max-width: 1024px)');
-      let desktop = window.matchMedia('(min-width: 1025px)');
-  
-      // Enable (for mobile)
-      if(desktop.matches) {
-          if (!init) {
-              init = true;
-              
+  let slideOne = document.getElementById('slide--one');
+  slideOne.addEventListener('click', ()=>{
+    swiperCs.slideTo(0);
+  })
 
-              swiperCs = new Swiper('.swiper--cs', {
-                direction: 'vertical',
-                noSwipingClass: 'cs__video__image-container',
-                mousewheel: false,
-                speed: 800,
-                keyboard: {
-                    enabled: true,
-                  },
-                  effect: 'slide',
-                  parallax: true,
-              })
+  let slideTwo = document.getElementById('slide--two');
+  slideTwo.addEventListener('click', ()=>{
+    swiperCs.slideTo(1);
+  })
+
+  let slideThree = document.getElementById('slide--three');
+  slideThree.addEventListener('click', ()=>{
+    swiperCs.slideTo(2);
+  })
+
+  let slideFour = document.getElementById('slide--four');
+  slideFour.addEventListener('click', ()=>{
+    swiperCs.slideTo(3);
+  })
+
+  let slideFive = document.getElementById('slide--five');
+  slideFive.addEventListener('click', ()=>{
+    swiperCs.slideTo(4);
+  })
+
+  let slideSix = document.getElementById('slide--six');
+  slideSix.addEventListener('click', ()=>{
+    swiperCs.slideTo(5);
+  })
+
+  swiperCs.on('transitionStart', function () {
+      var videos = document.querySelectorAll('video');
+      Array.prototype.forEach.call(videos, function(video){
+      video.pause();
+      });
+    }); 
         
-        
-            let slideOne = document.getElementById('slide--one');
-            slideOne.addEventListener('click', ()=>{
-                swiperCs.slideTo(0);
-            })
-        
-            let slideTwo = document.getElementById('slide--two');
-            slideTwo.addEventListener('click', ()=>{
-                swiperCs.slideTo(1);
-            })
-        
-            let slideThree = document.getElementById('slide--three');
-            slideThree.addEventListener('click', ()=>{
-                swiperCs.slideTo(2);
-            })
-        
-            let slideFour = document.getElementById('slide--four');
-            slideFour.addEventListener('click', ()=>{
-                swiperCs.slideTo(3);
-            })
-        
-            let slideFive = document.getElementById('slide--five');
-            slideFive.addEventListener('click', ()=>{
-                swiperCs.slideTo(4);
-            })
-        
-            let slideSix = document.getElementById('slide--six');
-            slideSix.addEventListener('click', ()=>{
-                swiperCs.slideTo(5);
-            })
-        
-            swiperCs.on('transitionStart', function () {
-                var videos = document.querySelectorAll('video');
-                Array.prototype.forEach.call(videos, function(video){
-                video.pause();
-                });
-              }); 
-
-
-          }
-  
-      }
-  
-      // Disable (for tablet)
-      else if(tablet.matches) {
-        swiperCs.destroy();
-          init = false;
-      }
-  
-      // Disable (for desktop)
-      else if(mobile.matches) {
-        swiperCs.destroy();
-          init = false;
-      }
-  }
-
-  
-  window.addEventListener('load', function() {
-    swiperMode();
-});
-
-/* On Resize
-**************************************************************/
-window.addEventListener('resize', function() {
-    swiperMode();
-});
-
-
-
    
 
 }
