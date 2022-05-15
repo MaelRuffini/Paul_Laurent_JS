4136b1db401c29b3ce6cc08e64c5152342d4dc12
MaelRuffini <mael.lucien.ruffini@gmail.com>
2022-05-15 15:38:31 +0200

    commit

--- src/pages/cs.js
+++ src/pages/cs.js
@@ -23,55 +23,106 @@ function caseStudy()
         keyboard: true,
     })
 
-    const swiperCs = new Swiper('.swiper--cs', {
-        direction: 'vertical',
-        noSwipingClass: 'cs__video__image-container',
-        mousewheel: false,
-        speed: 800,
-        keyboard: {
-            enabled: true,
-          },
-          effect: 'slide',
-          parallax: true,
-      })
+
+    /* Swiper
+    **************************************************************/
+    var swiperCs = Swiper;
+    var init = false;
 
 
-    let slideOne = document.getElementById('slide--one');
-    slideOne.addEventListener('click', ()=>{
-        swiperCs.slideTo(0);
-    })
+    function swiperMode() {
+      let mobile = window.matchMedia('(min-width: 0px) and (max-width: 768px)');
+      let tablet = window.matchMedia('(min-width: 769px) and (max-width: 1024px)');
+      let desktop = window.matchMedia('(min-width: 1025px)');
+  
+      // Enable (for mobile)
+      if(desktop.matches) {
+          if (!init) {
+              init = true;
+              
 
-    let slideTwo = document.getElementById('slide--two');
-    slideTwo.addEventListener('click', ()=>{
-        swiperCs.slideTo(1);
-    })
+              swiperCs = new Swiper('.swiper--cs', {
+                direction: 'vertical',
+                noSwipingClass: 'cs__video__image-container',
+                mousewheel: false,
+                speed: 800,
+                keyboard: {
+                    enabled: true,
+                  },
+                  effect: 'slide',
+                  parallax: true,
+              })
+        
+        
+            let slideOne = document.getElementById('slide--one');
+            slideOne.addEventListener('click', ()=>{
+                swiperCs.slideTo(0);
+            })
+        
+            let slideTwo = document.getElementById('slide--two');
+            slideTwo.addEventListener('click', ()=>{
+                swiperCs.slideTo(1);
+            })
+        
+            let slideThree = document.getElementById('slide--three');
+            slideThree.addEventListener('click', ()=>{
+                swiperCs.slideTo(2);
+            })
+        
+            let slideFour = document.getElementById('slide--four');
+            slideFour.addEventListener('click', ()=>{
+                swiperCs.slideTo(3);
+            })
+        
+            let slideFive = document.getElementById('slide--five');
+            slideFive.addEventListener('click', ()=>{
+                swiperCs.slideTo(4);
+            })
+        
+            let slideSix = document.getElementById('slide--six');
+            slideSix.addEventListener('click', ()=>{
+                swiperCs.slideTo(5);
+            })
+        
+            swiperCs.on('transitionStart', function () {
+                var videos = document.querySelectorAll('video');
+                Array.prototype.forEach.call(videos, function(video){
+                video.pause();
+                });
+              }); 
 
-    let slideThree = document.getElementById('slide--three');
-    slideThree.addEventListener('click', ()=>{
-        swiperCs.slideTo(2);
-    })
 
-    let slideFour = document.getElementById('slide--four');
-    slideFour.addEventListener('click', ()=>{
-        swiperCs.slideTo(3);
-    })
+          }
+  
+      }
+  
+      // Disable (for tablet)
+      else if(tablet.matches) {
+        swiperCs.destroy();
+          init = false;
+      }
+  
+      // Disable (for desktop)
+      else if(mobile.matches) {
+        swiperCs.destroy();
+          init = false;
+      }
+  }
 
-    let slideFive = document.getElementById('slide--five');
-    slideFive.addEventListener('click', ()=>{
-        swiperCs.slideTo(4);
-    })
+  
+  window.addEventListener('load', function() {
+    swiperMode();
+});
 
-    let slideSix = document.getElementById('slide--six');
-    slideSix.addEventListener('click', ()=>{
-        swiperCs.slideTo(5);
-    })
+/* On Resize
+**************************************************************/
+window.addEventListener('resize', function() {
+    swiperMode();
+});
 
-    swiperCs.on('transitionStart', function () {
-        var videos = document.querySelectorAll('video');
-        Array.prototype.forEach.call(videos, function(video){
-        video.pause();
-        });
-      });    
+
+
+   
 
 }
 
---
+++ src/pages/csBijs.js
@@ -0,0 +1,83 @@
+// import LocomotiveScroll from 'locomotive-scroll'
+// import Plyr from 'plyr';
+// import Swiper from 'swiper/bundle'
+// import 'swiper/css/bundle'
+// import gsap from "gsap";
+
+// function caseStudy() 
+// {
+
+//   const locoScroll = new LocomotiveScroll({
+//     el: document.querySelector('.main'),
+//     smooth: true,
+//     smartphone: {
+//       smooth: false
+//     },
+//     tablet: {
+//       smooth: true
+//     },
+//   }); 
+
+//       const player = new Plyr('#player',{
+//         clickToPlay: true,
+//         keyboard: true,
+//     })
+
+//     const swiperCs = new Swiper('.swiper--cs', {
+//         direction: 'vertical',
+//         noSwipingClass: 'cs__video__image-container',
+//         mousewheel: false,
+//         speed: 800,
+//         keyboard: {
+//             enabled: true,
+//           },
+//           effect: 'slide',
+//           parallax: true,
+//       })
+
+
+//     let slideOne = document.getElementById('slide--one');
+//     slideOne.addEventListener('click', ()=>{
+//         swiperCs.slideTo(0);
+//     })
+
+//     let slideTwo = document.getElementById('slide--two');
+//     slideTwo.addEventListener('click', ()=>{
+//         swiperCs.slideTo(1);
+//     })
+
+//     let slideThree = document.getElementById('slide--three');
+//     slideThree.addEventListener('click', ()=>{
+//         swiperCs.slideTo(2);
+//     })
+
+//     let slideFour = document.getElementById('slide--four');
+//     slideFour.addEventListener('click', ()=>{
+//         swiperCs.slideTo(3);
+//     })
+
+//     let slideFive = document.getElementById('slide--five');
+//     slideFive.addEventListener('click', ()=>{
+//         swiperCs.slideTo(4);
+//     })
+
+//     let slideSix = document.getElementById('slide--six');
+//     slideSix.addEventListener('click', ()=>{
+//         swiperCs.slideTo(5);
+//     })
+
+//     swiperCs.on('transitionStart', function () {
+//         var videos = document.querySelectorAll('video');
+//         Array.prototype.forEach.call(videos, function(video){
+//         video.pause();
+//         });
+//       });    
+
+// }
+
+// export default caseStudy
+
+
+
+
+
